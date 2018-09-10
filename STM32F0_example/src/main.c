c103f7ba4eb551c76ba3ce744e5f64fcf89e173d
Dat Pham <datpham7987@gmail.com>
2018-09-11 06:49:35 +0700

    code tiep hieu ung 1
    code tiep hieu ung 1

---
+++ STM32F0_example/src/main.c
@@ -0,0 +1,117 @@
+
+
+#include "stm32f0xx.h"
+#include"stm32f0xx_gpio.h"
+#include"stm32f0xx_rcc.h"
+#include"stm32f0xx_exti.h"
+#include"stm32f0xx_misc.h"
+#include"stm32f0xx_syscfg.h"
+#include"stm32f0xx_usart.h"
+#include"stm32f0xx_spi.h"
+/*
+ *
+ */
+USART_InitTypeDef USART_InitStructure;
+GPIO_InitTypeDef  GPIO_InitStructure;
+EXTI_InitTypeDef  EXTI_InitStructure;
+NVIC_InitTypeDef  NVIC_InitStructure;
+SPI_InitTypeDef  SPI_InitStruct;
+/*
+ *
+ */
+#define LED GPIO_Pin_4
+#define SDI GPIO_Pin_1
+#define SCK GPIO_Pin_2
+#define LATCH GPIO_Pin_3
+/*
+ *
+ */
+uint16_t x = 0x01;
+
+void Testled(uint32_t timer);
+void HC595_Latch(void);
+void Shift_Out(uint8_t Data);
+void HC595_Pulse(void);
+void Led_1(void);
+int main(void)
+{
+//	/*
+//	 * ================================================================SYSTEM_CONFIG_CLOCK_HSI================================================
+//	 */
+//	SystemInit();
+//	RCC_HSEConfig(RCC_HSE_OFF);
+//	RCC_HSICmd(ENABLE);
+//	RCC_PLLCmd(DISABLE);
+//	RCC_SYSCLKConfig(RCC_SYSCLKSource_HSI);
+	/*
+	 * 	==========================================SETUP GPIO================================
+	*/
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	GPIO_InitStructure.GPIO_Pin = LED|SDI|SCK|LATCH;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_ResetBits(GPIOA,LED|SDI|SCK|LATCH);
+	while(1)
+	{
+		Led_1();
+	}
+}
+/*
+ *===============================================HIEU UNG 1 =================================
+ *===========================================================================================
+ */
+void Led_1(void)
+{
+	Shift_Out(x);
+	Shift_Out(x);
+	HC595_Latch();
+	for(uint32_t i = 0; i < 5000000; i++)
+	{
+
+	}
+	x = x << 1;
+	if(x > 0x80)
+	{
+		x = 0x01;
+	}
+}
+
+/*
+ *
+ */
+
+/*
+ * ======================================================HC595===============================================
+ * ==========================================================================================================
+ * ==========================================================================================================
+ */
+void Shift_Out(uint8_t Data)
+{
+	for(uint8_t i = 0; i < 8; i++)
+	{
+		if(Data & 0x80)
+		{
+			GPIO_SetBits(GPIOA,SDI); // Data High
+		}
+		else
+		{
+			GPIO_ResetBits(GPIOA,SDI); // Data Low
+		}
+		HC595_Pulse();
+		Data = Data << 1;
+	}
+}
+
+void HC595_Pulse(void)
+{
+
+	GPIO_SetBits(GPIOA,SCK); // Clock High
+
+	GPIO_ResetBits(GPIOA,SCK); // Clock Low
+}
+void HC595_Latch(void)
+{
+	  GPIO_SetBits(GPIOA,LATCH); // Latch High
+	  GPIO_ResetBits(GPIOA,LATCH); // Latch Low
+}
